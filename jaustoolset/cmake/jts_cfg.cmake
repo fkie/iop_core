1aca8f286ca9b96d4c15110717dbccee7eba8706
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2019-04-25 09:52:57 +0200

    fixed build with java 11
    Added JAXB libraries to the repositories since they are removed from
    standard JDK library

--- jaustoolset/cmake/jts_cfg.cmake
+++ jaustoolset/cmake/jts_cfg.cmake
@@ -12,11 +12,25 @@ endif()
 get_filename_component(JTS_LIB_DIR "${jaustoolset_LIBRARIES}" PATH)
 set(jaustoolset_JAR ${JTS_LIB_DIR}/iop_codegenerator.jar)
 
-set(jaustoolset_INCLUDE_JARS
+set(jaustoolset_INCLUDE_JARS_LT11
     ${jaustoolset_WORKING_DIR}/libs_external/jargs-1.0/jargs.jar:${jaustoolset_WORKING_DIR}/libs_external/runtime/commons-lang-2.5.jar:${jaustoolset_WORKING_DIR}/libs_external/smc/Smc.jar
 )
-set(INCLUDE_LOCAL_JARS
+set(jaustoolset_INCLUDE_JARS_S11
+    ${jaustoolset_WORKING_DIR}/libs_external/jargs-1.0/jargs.jar:${jaustoolset_WORKING_DIR}/libs_external/runtime/commons-lang-2.5.jar:${jaustoolset_WORKING_DIR}/libs_external/smc/Smc.jar:${jaustoolset_WORKING_DIR}/libs_external/jaxb/javax.activation-api-1.2.0:${jaustoolset_WORKING_DIR}/libs_external/jaxb/jaxb-api-2.4.0-b180830.0359.jar:${jaustoolset_WORKING_DIR}/libs_external/jaxb/jaxb-core-2.3.0.1.jar:${jaustoolset_WORKING_DIR}/libs_external/jaxb/jaxb-impl-2.4.0-b180830.0438.jar
+)
+
+set(INCLUDE_LOCAL_JARS_LT11
     libs_external/jargs-1.0/jargs.jar
     libs_external/runtime/commons-lang-2.5.jar
     libs_external/smc/Smc.jar
+)
+
+set(INCLUDE_LOCAL_JARS_S11
+    libs_external/jargs-1.0/jargs.jar
+    libs_external/runtime/commons-lang-2.5.jar
+    libs_external/smc/Smc.jar
+    libs_external/jaxb/javax.activation-api-1.2.0.jar
+    libs_external/jaxb/jaxb-api-2.4.0-b180830.0359.jar
+    libs_external/jaxb/jaxb-core-2.3.0.1.jar
+    libs_external/jaxb/jaxb-impl-2.4.0-b180830.0438.jar
 )
